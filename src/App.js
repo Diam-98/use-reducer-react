9731ce2ae83ef75fc2c13c8765c4bea7a5dca221
Diam Diallo <70403651+Diam-98@users.noreply.github.com>
2023-07-16 22:10:49 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import people from "./data.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1> Utilisation de useReducer</h1>
+        <hr />
+        {people.map((item) => {
+          return (
+            <div className="user_list" key={item.id}>
+              <h4>{item.name}</h4>
+              <button>Effacer</button>
+            </div>
+          );
+        })}
+        <hr />
+        <button>Effacer tout</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,9 @@
+const people = [
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Smith" },
+  { id: 3, name: "Michael Johnson" },
+  { id: 4, name: "Emily Davis" },
+  { id: 5, name: "David Brown" },
+];
+
+export default people;
