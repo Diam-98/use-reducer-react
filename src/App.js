455488e55029480dd773cf6c4ce50c48ca3d8ed4
Diam Diallo <70403651+Diam-98@users.noreply.github.com>
2023-07-17 15:45:14 +0000

    finish reducer stuff

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import React from "react";
-import people from "./data.js";
+import React, { useReducer } from "react";
+import { data } from "./data";
+
+const REMOVE_ITEM = "REMOVE_ITEM";
+const CLEAR_LIST = "CLEAR_LIST";
+const RESET_LIST = "RESET_LIST";
+
+const defaultState = {
+  people: data,
+};
+
+const reducer = (state, action) => {
+  console.log(action.type);
+  switch (action.type) {
+    case action.type === REMOVE_ITEM:
+      const newPeople = state.people.filter(
+        (person) => person.id !== action.payload.id
+      );
+      return { ...state, newPeople };
+    case action.type === CLEAR_LIST:
+      return { ...state, people: [] };
+    case action.type === RESET_LIST:
+      return { ...state, people: data };
+    default:
+      return { ...state, people: data };
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const remove = (id) => {
+    dispatch({ type: REMOVE_ITEM, payload: id });
+  };
+
+  const clearList = () => {
+    dispatch({ type: CLEAR_LIST });
+  };
+
+  const resetList = () => {
+    dispatch({ type: RESET_LIST });
+  };
+
+  console.log(state);
+
   return (
     <div className="App">
       <div className="container">
         <h1> Utilisation de useReducer</h1>
         <hr />
-        {people.map((item) => {
+        {state.people.map((item) => {
           return (
             <div className="user_list" key={item.id}>
               <h4>{item.name}</h4>
-              <button>Effacer</button>
+              <button onClick={() => remove(item.id)}>Effacer</button>
             </div>
           );
         })}
         <hr />
-        <button>Effacer tout</button>
+        {data.length < 1 ? (
+          <button onClick={resetList}>Reunitialiser</button>
+        ) : (
+          <button onClick={clearList}>Effacer tout</button>
+        )}
       </div>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,9 +1,7 @@
-const people = [
+export const data = [
   { id: 1, name: "John Doe" },
   { id: 2, name: "Jane Smith" },
   { id: 3, name: "Michael Johnson" },
   { id: 4, name: "Emily Davis" },
   { id: 5, name: "David Brown" },
 ];
-
-export default people;
